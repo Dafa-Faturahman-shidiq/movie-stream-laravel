ed1e90db42253b22c361c268641e870e5195c910
dafa <dafaxpplg3@gmail.com>
2026-02-17 11:12:48 +0700

    feat : Penambahan Midleware Untuk mengecek MemberShip

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckMembership;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+$movies = [
+    [
+        'title' => 'One piece Film Red',
+        'director' => 'Gorō Taniguchi',
+        'year' => 2022,
+    ],
+    [
+        'title' => 'Your Name',
+        'director' => 'Makoto Shinkai',
+        'year' => 2016,
+    ],
+    [
+        'title' => 'kimetsu no yaiba: mugen train',
+        'director' => 'Yuki Kajiura',
+        'year' => 2020,
+    ],
+];
+
+Route::get('/movies/{id}', function ($id) use ($movies) {
+    return $movies[$id];
+})->middleware(CheckMembership::class);
+
+Route::get('/pricing', function () {
+    return 'BELI MEMBERSHIP DULU BOSKU';
 });
